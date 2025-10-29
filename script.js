32eca9066140ecf43bc077c65c3ef60fcd14fae9
Gabryel556 <130873467+Gabryel556@users.noreply.github.com>
2025-10-29 01:13:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://b1096bdb11de.ngrok-free.app";
+const API_URL = "https://03c289594460.ngrok-free.app";
 const WEBSITE_API_KEY = "ag_b1ac536efcbe3e2972293ebeba9d044227e077bec317bc98e66d4ebc8a198ec8"; 
 const jogoLancado = true;
 let translations = {};

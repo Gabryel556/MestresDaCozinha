61c13c1f14b734f6c918800034170bce2b76da90
Gabryel556 <130873467+Gabryel556@users.noreply.github.com>
2026-01-11 14:29:36 -0300

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://precipitative-nonmotoring-michelina.ngrok-free.dev";
 
 function getAdminHeaders() {
     const key = sessionStorage.getItem("admin_api_key");
